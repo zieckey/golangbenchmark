57d63464d638f307d0cba417addb1e72c12ad81c
weizili.360.laptop <zieckey@gmail.com>
2015-01-06 19:08:41 +0800

    Fix wg.Done() donot be called

--- udp/client/main.go
+++ udp/client/main.go
@@ -1,14 +1,15 @@
 package main
 
 import (
+	"bytes"
 	"flag"
 	"fmt"
+	"github.com/funny/overall"
 	"net"
+	"os"
 	"sync"
 	"sync/atomic"
 	"time"
-	"bytes"
-	"github.com/funny/overall"
 )
 
 var concurrence = flag.Int("c", 1, "Number of multiple requests to make")
@@ -17,20 +18,21 @@ var verbosity = flag.Bool("v", false, "How much troubleshooting info to print")
 var echoVerify = flag.Bool("e", false, "the echo mode and verify the response data")
 var hostPort = flag.String("h", "127.0.0.1:1053", "The hostname and port of the udp server")
 var messageLen = flag.Int("l", 100, "The length of the send message")
+var timeoutMs = flag.Int("t", 100, "The timeout milliseconds")
 
 type Stat struct {
-	send int64
-	recv int64
-	sendsucc int64
-	sendfail int64
-	recvsucc int64
-	recvfail int64
+	send       int64
+	recv       int64
+	sendsucc   int64
+	sendfail   int64
+	recvsucc   int64
+	recvfail   int64
 	compareerr int64
 }
 
-func (s Stat)dump() string {
-	return fmt.Sprintf("send:%v recv:%v sendsucc:%v recvsucc:%v compareerr:%v",
-		s.send, s.recv, s.sendsucc, s.recvsucc, s.compareerr)
+func (s Stat) dump() string {
+	return fmt.Sprintf("send:%v recv:%v sendsucc:%v recvsucc:%v compareerr:%v sendfail:%v recvfail:%v",
+		s.send, s.recv, s.sendsucc, s.recvsucc, s.compareerr, s.sendfail, s.recvfail)
 }
 
 var stat Stat
@@ -45,15 +47,21 @@ func main() {
 	}
 
 	wg.Wait()
-	
+
 	fmt.Println(stat.dump())
-	
+
 	var buf bytes.Buffer
 	recoder.WriteCSV(&buf)
 	fmt.Println(buf.String())
+
+	if stat.compareerr != 0 || stat.recvfail != 0 || stat.sendfail != 0 {
+		os.Exit(1)
+	}
 }
 
-func request(wg* sync.WaitGroup, record* overall.TimeRecoder) {
+func request(wg *sync.WaitGroup, record *overall.TimeRecoder) {
+	defer wg.Done()
+
 	addr, err := net.ResolveUDPAddr("udp", *hostPort)
 	if err != nil {
 		fmt.Println("server address error. It MUST be a format like this hostname:port", err)
@@ -67,28 +75,31 @@ func request(wg* sync.WaitGroup, record* overall.TimeRecoder) {
 		return
 	}
 	defer socket.Close()
-	
+
 	msg := make([]byte, *messageLen)
 	for i := 0; i < *messageLen; i++ {
-		msg[i] = 'a' + byte(i) % 26;
+		msg[i] = 'a' + byte(i)%26
 	}
 
 	for n := 0; n < *numRequest; n++ {
 		t := time.Now()
-		
+
+		socket.SetDeadline(t.Add(time.Duration(100 * time.Millisecond)))
+
 		// send data to server
-		
 		_, err = socket.Write(msg)
 		if err != nil {
 			fmt.Println("send data error ", err)
+			atomic.AddInt64(&stat.sendfail, 1)
 			return
 		}
-		
+
 		// recv data from server
 		data := make([]byte, 1472)
 		read, remoteAddr, err := socket.ReadFromUDP(data)
 		if err != nil {
 			fmt.Println("recv data error ", err)
+			atomic.AddInt64(&stat.recvfail, 1)
 			return
 		}
 		data = data[:read]
@@ -97,18 +108,16 @@ func request(wg* sync.WaitGroup, record* overall.TimeRecoder) {
 		if *verbosity {
 			fmt.Printf("server addr [%v], response data len:%v [%s]\n", remoteAddr, read, string(data[:read]))
 		}
-		
+
 		if *echoVerify {
 			if bytes.Compare(data, msg) != 0 {
 				atomic.AddInt64(&stat.compareerr, 1)
 			}
 		}
-		
+
 		atomic.AddInt64(&stat.send, 1)
 		atomic.AddInt64(&stat.recv, 1)
 		atomic.AddInt64(&stat.sendsucc, 1)
 		atomic.AddInt64(&stat.recvsucc, 1)
 	}
-
-	wg.Done()
 }
